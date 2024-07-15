36d8101fadb918d96f99b3228de9c1f3e99d0777
Edward Andrews-Hodgson <edward@aldreth.com>
2024-07-15 21:55:17 +0100

    Remove lots of posts and pages, personalise other stuff

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,31 +1,11 @@
 export default {
   top: [
     {
-      text: 'About',
-      url: '/about/'
-    },
-    {
-      text: 'Docs',
-      url: '/get-started/'
-    },
-    {
-      text: 'Built with',
-      url: '/built-with/'
-    },
-    {
-      text: 'Blog',
-      url: '/blog/'
+      text: 'Stories',
+      url: '/stories/'
     }
   ],
   bottom: [
-    {
-      text: 'Style guide',
-      url: '/styleguide/'
-    },
-    {
-      text: 'Imprint',
-      url: '/imprint/'
-    },
     {
       text: 'Privacy',
       url: '/privacy/'
